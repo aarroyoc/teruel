87ef66c51bd789c29bc0ba121d11cad5034d7405
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2021-11-04 22:31:47 +0100

    Example: Dice

---
+++ examples/dice/README.md
@@ -0,0 +1,15 @@
+# Dice Sample
+
+This example demonstrates how to integrate the Scryer HTTP server with Teruel and htmx.
+
+![Dice demo](Dice.gif)
+
+Running it:
+
+```
+scryer-prolog app.pl
+
+?- listen(8080).
+```
+
+Visit http://localhost:8080
---
+++ examples/dice/app.pl
@@ -0,0 +1,26 @@
+:- use_module(library(random)).
+:- use_module(lyncex).
+:- use_module('../../teruel.pl').
+
+listen(Port) :-
+    http_listen(Port, [
+        get(/, index),
+        get('dice.svg', dice),
+        get('htmx.min.js', htmx)
+    ]).
+
+index(_Request, Response) :-
+    http_headers(Response, ["content-type"-"text/html"]),
+    http_body(Response, file("index.html")).
+
+dice(_Request, Response) :-
+    random_integer(1, 7, N),
+    render("dice.svg", ["n"-N], DiceSvg),
+    http_status_code(Response, 200),
+    http_headers(Response, ["content-type"-"image/svg+xml"]),
+    http_body(Response, text_custom(DiceSvg)).
+
+htmx(_Request, Response) :-
+    http_status_code(Response, 200),
+    http_headers(Response, ["content-type"-"text/javascript"]),
+    http_body(Response, file("htmx.min.js")).
